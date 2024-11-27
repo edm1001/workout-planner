f80c3eb318fcb705651407a4d80dc5031fb2e676
Edmer Valencia <edmerfranciz@gmail.com>
2024-11-26 23:58:44 -0600

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/Footer';
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer";
+import Home from "./pages/Home.jsx";
+import Planner from "./pages/Planner.jsx";
+import WorkoutLibrary from "./pages/WorkoutLibrary.jsx";
 
-
-function App() {
-<>
-<Navbar/>
-<Footer/>
-</>
-
-
+const App = () => {
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/planner" element={<Planner />} />
+      <Route path="/library" element={<WorkoutLibrary />} />
+    </Routes>
+    <Footer />
+  </>;
 }
 
-export default App
+export default App;
