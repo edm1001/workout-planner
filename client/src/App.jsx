245c50e8a4e6785aa36a7d657037387e97f7a463
Edmer Valencia <edmerfranciz@gmail.com>
2024-11-28 00:02:12 -0600

    fix render bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer";
 import Home from "./pages/Home.jsx";
 import Planner from "./pages/Planner.jsx";
 import WorkoutLibrary from "./pages/WorkoutLibrary.jsx";
 
+console.log("App");
+
 const App = () => {
-  <>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/planner" element={<Planner />} />
-      <Route path="/library" element={<WorkoutLibrary />} />
-    </Routes>
-    <Footer />
-  </>;
-}
+  return (
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/planner" element={<Planner />} />
+          <Route path="/library" element={<WorkoutLibrary />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
